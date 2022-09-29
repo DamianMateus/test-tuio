6dcd12fe767d7a0e8896ad73391b148c79836e5e
DamianMateus <willpxr1@gmail.com>
2022-09-29 04:07:01 -0500

    add footer

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import classes from "./Footer.module.scss";
+import { Link, useNavigate } from "react-router-dom";
+import twitter from "./../../assets/icon-twitter.png";
+import facebook from "./../../assets/icon-facebook.png";
+import logo from "./../../assets/logo.png";
 
 export default function Footer() {
   return (
-    <div>
-      footer
-    </div>
-  )
+    <footer className={classes.footer}>
+      <div className={classes.footer__content}>
+        <img
+          src={logo}
+          alt="TuioLogo"
+          className={classes.footer__content__logo}
+        ></img>
+        <div className={classes.footer__content__socialMedia}>
+          <img
+            src={facebook}
+            alt="facebookLogo"
+            className={classes.footer__content__socialMedia__facebook}
+          ></img>
+          <img
+            src={twitter}
+            alt="twitterLogo"
+            className={classes.footer__content__socialMedia__twitter}
+          ></img>
+        </div>
+
+        <ul>
+          <li>
+            <Link to="/">Politica de tratamiento de datos</Link>
+          </li>
+          <li>
+            <Link to="/">Terminos y condiciones</Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
---
+++ src/pages/users/Users.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import classes from "./Users.module.scss";
+
+const Users = () => {
+  return (
+    <>
+      <div className={classes.users}>
+
+      </div>
+    </>
+  );
+};
+
+export default Users;
